6e0fa73e7548f9922a53539ff06a6e2e0d0e488d
krgneha <neha224229@gmail.com>
2021-08-08 20:58:06 +0530

    assignment 7 and 8 is pending

---
+++ Lab_Assignment_8/applet1.java
@@ -0,0 +1,44 @@
+// import java.applet.*;
+// import java.awt.*;
+// import javax.swing.*;
+  
+// public class ellipse extends JApplet {
+  
+//     public void init()
+//     {
+//         // set size
+//         setSize(400, 400);
+  
+//         repaint();
+//     }
+  
+//     // paint the applet
+//     public void paint(Graphics g)
+//     {
+//         // set Color for rectangle
+//         g.setColor(Color.red);
+  
+//         // draw a ellipse
+//         g.drawOval(100, 100, 150, 100);
+//     }
+// }
+
+
+
+import java.applet.*;
+import java.awt.*;
+public class GApplet extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawLine(20, 20, 500, 20);
+		g.drawRect(20, 40, 200, 40);
+		g.fillRect(300, 40, 200, 40);
+		g.drawRoundRect(20, 100, 200, 40, 10, 10);
+		g.fillRoundRect(300, 100, 200, 40, 10, 10);
+		g.setColor(Color.RED);
+		g.drawOval(20, 160, 200, 100);
+		g.fillOval(300, 160, 200, 100);
+	}
+}
+
