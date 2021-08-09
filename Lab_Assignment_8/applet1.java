1cc67c14ac6fa69aa8510897e2b837b6787dc80f
krgneha <neha224229@gmail.com>
2021-08-09 20:13:48 +0530

    Assignment 7 is done

--- Lab_Assignment_8/applet1.java
+++ Lab_Assignment_8/applet1.java
@@ -25,20 +25,20 @@
 
 
 
-import java.applet.*;
-import java.awt.*;
-public class GApplet extends Applet
-{
-	public void paint(Graphics g)
-	{
-		g.drawLine(20, 20, 500, 20);
-		g.drawRect(20, 40, 200, 40);
-		g.fillRect(300, 40, 200, 40);
-		g.drawRoundRect(20, 100, 200, 40, 10, 10);
-		g.fillRoundRect(300, 100, 200, 40, 10, 10);
-		g.setColor(Color.RED);
-		g.drawOval(20, 160, 200, 100);
-		g.fillOval(300, 160, 200, 100);
-	}
-}
+// import java.applet.*;
+// import java.awt.*;
+// public class GApplet extends Applet
+// {
+// 	public void paint(Graphics g)
+// 	{
+// 		g.drawLine(20, 20, 500, 20);
+// 		g.drawRect(20, 40, 200, 40);
+// 		g.fillRect(300, 40, 200, 40);
+// 		g.drawRoundRect(20, 100, 200, 40, 10, 10);
+// 		g.fillRoundRect(300, 100, 200, 40, 10, 10);
+// 		g.setColor(Color.RED);
+// 		g.drawOval(20, 160, 200, 100);
+// 		g.fillOval(300, 160, 200, 100);
+// 	}
+// }
 
