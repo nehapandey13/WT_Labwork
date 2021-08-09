ab297dff28692ceee391d1227e9dd6940cac0551
krgneha <neha224229@gmail.com>
2021-08-09 20:25:54 +0530

    Assignment 8 is done

--- Lab_Assignment_8/applet1.java
+++ Lab_Assignment_8/applet1.java
@@ -1,44 +1,27 @@
-// import java.applet.*;
-// import java.awt.*;
-// import javax.swing.*;
-  
-// public class ellipse extends JApplet {
-  
-//     public void init()
-//     {
-//         // set size
-//         setSize(400, 400);
-  
-//         repaint();
-//     }
-  
-//     // paint the applet
-//     public void paint(Graphics g)
-//     {
-//         // set Color for rectangle
-//         g.setColor(Color.red);
-  
-//         // draw a ellipse
-//         g.drawOval(100, 100, 150, 100);
-//     }
-// }
 
+import java.applet.*;
+import java.awt.*;
+import javax.swing.*;
+public class applet1 extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawLine(20, 20, 500, 20);
+		g.drawRect(20, 40, 200, 40);
+		g.fillRect(300, 40, 200, 40);
+		g.drawRoundRect(20, 100, 200, 40, 10, 10);
+		g.fillRoundRect(300, 100, 200, 40, 10, 10);
+		g.setColor(Color.RED);
+		g.drawOval(20, 160, 200, 100);
+		g.fillOval(300, 160, 200, 100);
+	}
 
-
-// import java.applet.*;
-// import java.awt.*;
-// public class GApplet extends Applet
-// {
-// 	public void paint(Graphics g)
-// 	{
-// 		g.drawLine(20, 20, 500, 20);
-// 		g.drawRect(20, 40, 200, 40);
-// 		g.fillRect(300, 40, 200, 40);
-// 		g.drawRoundRect(20, 100, 200, 40, 10, 10);
-// 		g.fillRoundRect(300, 100, 200, 40, 10, 10);
-// 		g.setColor(Color.RED);
-// 		g.drawOval(20, 160, 200, 100);
-// 		g.fillOval(300, 160, 200, 100);
-// 	}
-// }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        applet1 Gfx = new applet1();
+        frame.getContentPane().add(Gfx, BorderLayout.CENTER);
+        frame.setSize(new Dimension(550, 550));
+        frame.setVisible(true);
+    }
+}
 
